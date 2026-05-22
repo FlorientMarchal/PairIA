ec1dcf7b748e980d59e5cdea826f2bd31c4983b9
Kardiatou102 <kardiatou22.ba@gmail.com>
2026-05-22 10:29:00 +0200

    ajout des icônes de suppression de l'hitorique de conversation

--- site/shell.php
+++ site/shell.php
@@ -10,6 +10,7 @@ require_once 'includes/bd.php';
   <title>PairIA</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link href="https://fonts.googleapis.com/css2?family=Syne:wght@600;700;800&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;1,9..40,300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
   <link rel="stylesheet" href="styles/global.css">
   <link rel="stylesheet" href="styles/index.css">
   <link rel="stylesheet" href="styles/article.css">
